f1c03dc59d8e15193b142b3243f17eb00cfd446c
Florian Viorel Tanely <izkx73@gmail.com>
2025-12-19 22:34:04 +0700

    Add mark all unread notifications as read request

--- src/main/java/teammates/ui/request/MarkAllUnreadNotificationsAsReadRequest.java
+++ src/main/java/teammates/ui/request/MarkAllUnreadNotificationsAsReadRequest.java
@@ -3,6 +3,9 @@ package teammates.ui.request;
 /**
  * The request of marking all unread notification as read in account.
  */
-public class MarkAllUnreadNotificationsAsReadRequest {
-
+public class MarkAllUnreadNotificationsAsReadRequest extends BasicRequest {
+    @Override
+    public void validate() throws InvalidHttpRequestBodyException {
+        // Nothing to validate
+    }
 }
